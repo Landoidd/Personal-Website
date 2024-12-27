ed29bd6060b7b91a9e7beb8c7a8ed89494178679
Landon Zigler <landonzigler@gmail.com>
2024-12-27 13:14:57 -0500

    Added mobile layout to navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+"use client"
+import {useState} from "react";
 import "./globals.css";
 import Link from "next/link";
+import HamburgerIcon from "../../public/images/hamburger_icon";
 
 /*
 Create default Layout of Personal Website
  */
 export default function Layout({children}: { children: React.ReactNode }) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    }
     return (
         <html lang="en" className="scroll-smooth font-sans">
         <body className="flex flex-col min-h-screen bg-gradient-to-b from-zinc-800 to-zinc-900 text-white">
@@ -19,7 +26,10 @@ export default function Layout({children}: { children: React.ReactNode }) {
                 <header className="p-4 font-bold bg-white/10 border-b-2 border-white/30">
                     {/*Navbar*/}
                     <nav className="container mx-auto justify-around">
-                        <ul className="flex flex-wrap justify-around font-sans">
+                        {/* Define large and small screen size layout for navbar */}
+                        <ul
+                            className={`flex flex-col gap-2 items-center font-sans md:flex md:flex-row md:justify-around ${isOpen ? "block" : "hidden"}`}
+                        >
                             <li><Link className="hover:underline cursor-pointer" href="/">Home</Link></li>
                             <li><Link className="hover:underline cursor-pointer" href="/work-experience">Work
                                 Experience</Link></li>
@@ -30,6 +40,12 @@ export default function Layout({children}: { children: React.ReactNode }) {
                             <li><Link className="hover:underline cursor-pointer" href="/connect">Connect</Link></li>
                         </ul>
                     </nav>
+                    {/* Hamburger Icon */}
+                    <div className="flex md:hidden justify-start items-center" onClick={toggleMenu}>
+                        <button className="text-white">
+                          <HamburgerIcon/>
+                        </button>
+                    </div>
                 </header>
                 {/*End of Header*/}
 
