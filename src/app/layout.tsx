9e7f30eb1cf48e9151be0cf239cd29155014f369
Landon Zigler <landonzigler@gmail.com>
2024-12-27 12:30:42 -0500

    Removed sidebars for smaller screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,34 +9,39 @@ export default function Layout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en" className="scroll-smooth font-sans">
         <body className="flex flex-col min-h-screen bg-gradient-to-b from-zinc-800 to-zinc-900 text-white">
-        <div className="flex flex-grow">
-        {/* Left Sidebar */}
-        <div className="w-1/5"></div>
-        {/* Main Content */}
-        <div className="w-3/5 main-content flex flex-col">
-            {/*Header*/}
-            <header className="p-4 font-bold bg-white/10 border-b-2 border-white/30">
-                {/*Navbar*/}
-                <nav className="container mx-auto justify-around">
-                    <ul className="flex flex-wrap justify-around font-sans">
-                        <li><Link className="hover:underline cursor-pointer" href="/">Home</Link></li>
-                        <li><Link className="hover:underline cursor-pointer" href="/work-experience">Work Experience</Link></li>
-                        <li><Link className="hover:underline cursor-pointer" href="/technical-skills">Technical Skills</Link></li>
-                        <li><Link className="hover:underline cursor-pointer" href="/projects">Projects</Link>
-                        </li>
-                        <li><Link className="hover:underline cursor-pointer" href="/connect">Connect</Link></li>
-                    </ul>
-                </nav>
-            </header>
-            {/*End of Header*/}
+        <div className="flex flex-grow w-full">
+            {/* Left Sidebar */}
+            <div className="hidden md:block w-1/5"></div>
 
-            {/* Main */}
-            <main className="flex-grow m-4 animate-fadeInUp opacity-100">{children}</main>
-            {/* End of Main */}
+            {/* Main Content */}
+            <div className="w-full md:w-3/5 main-content flex flex-col">
+                {/*Header*/}
+                <header className="p-4 font-bold bg-white/10 border-b-2 border-white/30">
+                    {/*Navbar*/}
+                    <nav className="container mx-auto justify-around">
+                        <ul className="flex flex-wrap justify-around font-sans">
+                            <li><Link className="hover:underline cursor-pointer" href="/">Home</Link></li>
+                            <li><Link className="hover:underline cursor-pointer" href="/work-experience">Work
+                                Experience</Link></li>
+                            <li><Link className="hover:underline cursor-pointer" href="/technical-skills">Technical
+                                Skills</Link></li>
+                            <li><Link className="hover:underline cursor-pointer" href="/projects">Projects</Link>
+                            </li>
+                            <li><Link className="hover:underline cursor-pointer" href="/connect">Connect</Link></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/*End of Header*/}
+
+                {/* Main */}
+                <main className="flex-grow m-4 animate-fadeInUp opacity-100">{children}</main>
+                {/* End of Main */}
+            </div>
+
+            {/* Right Sidebar */}
+            <div className="hidden md:block w-1/5"></div>
         </div>
-        </div>
-        {/* Right Sidebar */}
-        <div className="w-1/5"></div>
+
         {/* Footer */}
         <footer className="text-center text-xs">
             © 2024 Landon Zigler. All rights reserved.
